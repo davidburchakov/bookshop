712b45163bde2dd25fcb529d879c871858d6f017
David <davidburchakov@gmail.com>
2024-01-04 01:35:48 +0100

    using cookies with AJAX

--- shopapp/views/views.py
+++ shopapp/views/views.py
@@ -94,11 +94,9 @@ def process_purchase(request):
 
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
 import json
 
 @require_POST
-@csrf_exempt
 def update_cart(request):
     try:
         data = json.loads(request.body)
