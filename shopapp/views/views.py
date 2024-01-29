59189d84b9d683c78cee08497abdcbb17b0cd310
David <davidburchakov@gmail.com>
2024-01-29 18:05:38 +0100

    added empty cards if books not available

--- shopapp/views/views.py
+++ shopapp/views/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .chatbot_view import get_recommended_books, get_most_popular_books
 from ..models.models import Faq
 
+
 def get_all_faq():
     faqs = Faq.objects.all()
     faq_list = [{"question": faq.question, "answer": faq.answer} for faq in faqs]
@@ -11,6 +12,8 @@ def get_all_faq():
 
 from ..models.models import Books
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
 def get_all_books_paginator(page_num=1, books_per_page=30):
     books_qs = Books.objects.all().prefetch_related('authors').order_by('id')
     paginator = Paginator(books_qs, books_per_page)
@@ -38,8 +41,6 @@ def get_all_books_paginator(page_num=1, books_per_page=30):
     return books
 
 
-
-
 def index_view(request: HttpRequest):
     page = request.GET.get('page', 1)  # Get the page number from query params
     most_popular_books = get_most_popular_books()
@@ -48,7 +49,9 @@ def index_view(request: HttpRequest):
     context = {
         "books_page": books_page,  # This is already a paginated list of books
         "recommended_books": recommended_books,
-        "most_popular_books": most_popular_books
+        "most_popular_books": most_popular_books,
+        "range": range(1, 11),
+        "range_30": range(1, 31)
     }
     # Check if the request is AJAX
     # Detect AJAX request
@@ -58,8 +61,6 @@ def index_view(request: HttpRequest):
         return render(request, 'shopapp/books_list.html', {"books_page": books_page})
 
     return render(request, 'shopapp/shop-index.html', context=context)
-
-
 
 
 def about_view(request: HttpRequest):
@@ -76,5 +77,3 @@ def faq_view(request: HttpRequest):
 
 def seneka_pg1(request: HttpRequest):
     return render(request, 'books/seneka/seneka-read.html')
-
-
